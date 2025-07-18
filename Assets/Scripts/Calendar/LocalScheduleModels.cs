cc4f2dfea22f46e71269017f62c331c7dd3b33c2
Anastasia Altamirano <anapaltami@gmail.com>
2025-07-17 16:13:03 -1000

    Add ScheduledSession class with properties for task management

--- Assets/Scripts/Calendar/LocalScheduleModels.cs
+++ Assets/Scripts/Calendar/LocalScheduleModels.cs
@@ -1,2 +1,16 @@
 using SQLite;
 using System;
+
+[Table("scheduled_sessions")]
+public class ScheduledSession
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+    public string TaskTitle { get; set; }
+    public string TaskCategory { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public int BreakAfter { get; set; }
+}

52eec4bf3dfe04df18fddb3b5cf2eec1ac759895
Anastasia Altamirano <anapaltami@gmail.com>
2025-07-18 16:57:56 -1000

    Refactor ScheduleLocalStorage class to improve structure and ensure proper initialization of the local database

--- Assets/Scripts/Calendar/ScheduleLocalStorage.cs
+++ Assets/Scripts/Calendar/ScheduleLocalStorage.cs
@@ -3,3 +3,16 @@ using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 using SQLite;
+
+public class ScheduleLocalStorage : MonoBehaviour
+{
+    private static string DbPath => Path.Combine(Application.persistentDataPath, "UserSchedule.db");
+    private SQLiteConnection db;
+
+    void Awake()
+    {
+        db = new SQLiteConnection(DbPath);
+        db.CreateTable<ScheduledSession>();
+        Debug.Log("Local database initialized at: " + DbPath);
+    }
+}

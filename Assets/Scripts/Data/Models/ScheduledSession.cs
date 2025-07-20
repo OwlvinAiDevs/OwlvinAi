23bc5e6d3ebc1e6519ff3fd9a9eea544bbb8c59e
Anastasia Altamirano <anapaltami@gmail.com>
2025-07-20 12:33:10 -1000

    Refactor ScheduledSession class to remove MonoBehaviour inheritance and clean up unused methods

--- Assets/Scripts/Data/Models/ScheduledSession.cs
+++ Assets/Scripts/Data/Models/ScheduledSession.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
+using SQLite;
 
-public class ScheduledSession : MonoBehaviour
+[Table("scheduled_sessions")]
+public class ScheduledSession
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [PrimaryKey, AutoIncrement]
+    public int id { get; set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Indexed]
+    public int user_id { get; set; }
+    [Indexed]
+    public int task_id { get; set; }
+
+    public DateTime start_time { get; set; }
+    public DateTime end_time { get; set; }
+
+    public int break_after { get; set; } = 5;
 }

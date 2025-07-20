b5f768b4b7ea833e1f01fde76bce0936cf60fce3
Anastasia Altamirano <anapaltami@gmail.com>
2025-07-20 12:27:20 -1000

    Add initial implementation of database management and data models

---
+++ Assets/Scripts/Data/DatabaseManager.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using UnityEngine;
+using SQLite;
+using UnityEditor.VisionOS;
+
+public static class DatabaseManager
+{
+    public static SQLiteConnection db;
+
+    public static void Init(string dbName = "UserSchedule.db")
+    {
+        string dbPath = Path.Combine(Application.persistentDataPath, dbName);
+        db = new SQLiteConnection(dbPath);
+
+        db.CreateTable<User>();
+        db.CreateTable<Task>();
+        db.CreateTable<ScheduledSession>();
+        db.CreateTable<SessionLog>();
+        db.CreateTable<EnergyLevel>();
+        db.CreateTable<CachedAvailability>();
+        db.CreateTable<BlockedTime>();
+        db.CreateTable<AIResponse>();
+        db.CreateTable<AIChatLog>();
+
+        Debug.Log($"[SQLite] Database initialized at: {dbPath}");
+    }
+
+    public static void Close()
+    {
+        db?.Close();
+        Debug.Log("[SQLite] Database connection closed.");
+    }
+}

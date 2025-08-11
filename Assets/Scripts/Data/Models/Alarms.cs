b18373950a6387e5007eba6db7054e6e2685c8f1
Anastasia Altamirano <anapaltami@gmail.com>
2025-08-10 21:37:15 -1000

    Add AlarmModel class for managing user alarms in the database

---
+++ Assets/Scripts/Data/Models/Alarms.cs
@@ -0,0 +1,15 @@
+using SQLite;
+
+[Table("alarms")]
+public class AlarmModel
+{
+    [PrimaryKey, AutoIncrement]
+    public int id { get; set; }
+
+    [Indexed]
+    public int user_id { get; set; }
+
+    public int hour { get; set; }
+    public int minute { get; set; }
+    public bool is_enabled { get; set; } = true;
+}
